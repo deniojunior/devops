554fe1ae31f0adc750455fe145e62bc58c06e6b9
deniojunior <deniofriacamoreirajr@gmail.com>
2019-09-10 07:48:56 -0300

    Create workers and add them to cluster

--- swarm-cluster-aws-terraform/cluster-nodes.tf
+++ swarm-cluster-aws-terraform/cluster-nodes.tf
@@ -7,24 +7,63 @@ resource "aws_instance" "swarm-manager" {
     ami = "ami-2757f631"
     instance_type = "t2.micro"
     security_groups = ["${aws_security_group.swarm-cluster.name}"]
-    key_name = "${aws_key_pair.deployer.key_name}"
+    key_name = "${aws_key_pair.swarm-cluster-deployer.key_name}"
+
+    connection{
+        host = "${self.public_ip}"
+        user = "ubuntu"
+    }
+
+    provisioner "file" {
+        source = "swarm-cluster"
+        destination = "/home/ubuntu/swarm-cluster"
+    } 
 
     provisioner "remote-exec" {
         inline = [
-            "sudo apt-get update",
-            "curl -fsSL https://get.docker.com -o get-docker.sh",
-            "sh get-docker.sh",
+            "bash /home/ubuntu/swarm-cluster/install_docker.sh",
             "sudo docker swarm init",
             "sudo docker swarm join-token --quiet worker > /home/ubuntu/token"
         ]
     }
 
-#    provisioner "file" {
-#        source = "docker-swarm-files"
-#        destination = "/home/ubuntu/"
-#    } 
+    tags = {
+        Name = "swarm-cluster-manager"
+    }
+}
 
-    tags = { 
-        Name = "swarm-cluster"
+resource "aws_instance" "swarm-worker" {
+    count = 2
+    ami = "ami-2757f631"
+    instance_type = "t2.micro"
+    security_groups = ["${aws_security_group.swarm-cluster.name}"]
+    key_name = "${aws_key_pair.swarm-cluster-deployer.key_name}"
+
+    connection{
+        host = "${self.public_ip}"
+        user = "ubuntu"
+    }
+ 
+    provisioner "file" {
+        source = "swarm-cluster"
+        destination = "/home/ubuntu/swarm-cluster"
+    }
+
+    provisioner "file" {
+        source = "~/.ssh/id_rsa"
+        destination = "/home/ubuntu/.ssh/manager_key"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "bash /home/ubuntu/swarm-cluster/install_docker.sh",
+            "sudo chmod 400 ~/.ssh/manager_key",
+            "sudo scp -o StrictHostKeyChecking=no -o NoHostAuthenticationForLocalhost=yes -o UserKnownHostsFile=/dev/null -i ~/.ssh/manager_key ubuntu@${aws_instance.swarm-manager.private_ip}:/home/ubuntu/token .",
+            "sudo docker swarm join --token $(cat /home/ubuntu/token) ${aws_instance.swarm-manager.private_ip}:2377"
+        ]
+    }
+
+    tags = {
+        Name = "swarm-cluster-worker"
     }
 }
---
+++ swarm-cluster-aws-terraform/swarm-cluster/install_docker.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
